81a1bc80927243fc7b7829bff80b27fb0332d476
Chris Mahr <mahrtian@gmail.com>
2019-11-21 16:52:45 -0500

    create new App.js file

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>Welcome to Chris Mahr's portfolio website.</p>
+                <p>He's a software engineer who's always looking forward to working on meaningful projects.</p>
+            </div>
+        )
+    }
+}
+
+export default App
