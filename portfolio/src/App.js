080c1b55d0c154c32196c1417d5c5e76f7239bbb
Chris Mahr <mahrtian@gmail.com>
2019-11-25 15:15:33 -0500

    add code for Section 2, Lesson 11 (State)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
 
-class RegularClass {}
-class ComponentClass extends Component {}
-
-const regularClassInstance = new RegularClass()
-const componentClassInstance = new ComponentClass()
-
-console.log('regularClassInstance', regularClassInstance)
-console.log('componentClassInstance', componentClassInstance)
-
 class App extends Component {
+    constructor() {
+        super()
+        this.state = { 
+            displayBio: false 
+        }
+    }
+    
     render() {
         return (
             <div>
                 <h1>Hello!</h1>
                 <p>Welcome to Chris Mahr's portfolio website.</p>
                 <p>He's a software engineer who's always looking forward to working on meaningful projects.</p>
+                { 
+                    this.state.displayBio ? (
+                        <div>
+                            <p>He lives in Brooklyn, NY, and have been coding for the past four years.</p>
+                            <p>His favorite programming language is JavaScript. He's been using mostly Vue.js for the past two years, but has been teaching himself React.js more recently.</p>
+                            <p>Besides coding, he also loves to cook and play ultimate frisbee.</p>
+                        </div>
+                    ) : null 
+                }
             </div>
         )
     }
