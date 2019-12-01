feda6d1df3a27aab6167d8894958319958800073
Chris Mahr <mahrtian@gmail.com>
2019-12-01 17:15:11 -0500

    add data for links to social profiles

---
+++ portfolio/src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png'
+import githubIcon from '../assets/github_icon.png'
+import linkedinIcon from '../assets/linkedin_icon.png'
+import twitterIcon from '../assets/twitter_icon.png'
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:mahrtian@gmail.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'https://github.com/cmahrtian',
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: 'https://www.linkedin.com/in/cmahrtian',
+    image: linkedinIcon
+  },
+  {
+    id: 4,
+    link: 'https://twitter.com/cmahrtian',
+    image: twitterIcon
+  }
+]
+
+export default SOCIAL_PROFILES
